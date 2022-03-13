af31aed7a8901bc83f8bbf606376e20d883edece
OldBeliver <380506338004@yandex.ua>
2022-03-13 16:42:33 +0200

    Добавьте файлы проекта.

---
+++ RaisingToPower/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaisingToPower
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int value = 2;
+            int power = 0;
+            double calculatedNumber = 0;
+            int maxValue = 1024;
+
+            Random rand = new Random();
+            int givenNumber = rand.Next(maxValue);
+
+            bool doFind = true;
+
+            while (doFind)
+            {   
+                calculatedNumber = Math.Pow(value, power);
+                power++;
+
+                if (givenNumber < calculatedNumber)
+                {
+                    power--;
+                    doFind = false;                   
+                }                                
+            }
+            Console.WriteLine($"заданное число: {givenNumber}");
+            Console.WriteLine($"двойка в степени {power}\n{givenNumber.ToString("N0")}<{calculatedNumber.ToString("N0")}");
+        }
+    }
+}
